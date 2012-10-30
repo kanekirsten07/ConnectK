3b1950ec409cfaabd221eddcd905e6dfa7fcbfc0
Kirsten Kane <kanekirsten07@gmail.com>
2012-10-30 16:12:46 -0700

    Abstract Representation of Possible board States
    Added method stubs and constructor for Move.java, which is a class that
    will be used to represent possible move states that will be evaluated to
    be used in the Minimax algorithm

---
+++ src/Move.java
@@ -0,0 +1,21 @@
+
+public class Move {
+	
+	
+	private int [][]state;
+	public Move (int [][]boardstate)
+	{
+		this.state=boardstate;
+	}
+	
+	public void generateMoveGravityOn()
+	{
+		
+	}
+	
+	public void generateMoveGravityOff()
+	{
+		
+	}
+
+}
